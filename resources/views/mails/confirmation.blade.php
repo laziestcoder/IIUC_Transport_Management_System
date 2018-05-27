020773d7cc133b9c7ba3b8c0e2d4518318b46a31
Towfiqul Islam <towfiq.106@gmail.com>
2018-05-27 07:00:59 +0600

    all files removed for security reasons

--- resources/views/mails/confirmation.blade.php
+++
@@ -1,5 +0,0 @@
-Assalamu Alaikum.
-Dear {{$name}},
-Your registartion is complete. Please click the link below to active your account and confirm your email and get access.
-
-{{ route('confirmation', $token) }}
--- resources/views/pages/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="jumbotron text-center">
-    <h1> <?php echo  $title; ?> </h1>
-    <p>This is about page</p>
-</div> 
-@endsection
