5e07946b080898d6844fc5c166146e7d8195fc24
laziestcoder <towfiq.106@gmail.com>
2018-12-07 02:33:42 +0600

    7 th December 2018

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         //         $this->call(UsersTableSeeder::class);
         $this->call([
-          //  AdminTablesSeed::class,
+            AdminTablesSeed::class,
             ModelListTableSeeder::class,
         ]);
         // $this->runToo();
